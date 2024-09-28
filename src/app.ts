5ce2d96cf2362d4c5a6d261d54c81a068f712955
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-28 20:58:47 +0600

    first commit

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 const app = express()
 
 app.get('/', (req: Request, res: Response) => {
-  const a = 'bolo'
+  const a = 'Hello world'
 
   res.send(a)
 })
