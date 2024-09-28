69cab08f6d329a1d40a18e44b3a775d0aeb967c3
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-09-28 21:36:39 +0600

    global error middleware created

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import notFound from "./app/modules/middleware/notFound";
+import globalErrorHandler from "./app/modules/middleware/globalErrorHandler";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
@@ -6,5 +8,11 @@ app.get("/", (req: Request, res: Response) => {
 
   res.send(a);
 });
+
+// global error handler middleware
+app.use(globalErrorHandler);
+
+// not found route middleware
+app.use(notFound);
 
 export default app;
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   db_url: process.env.DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
 };
